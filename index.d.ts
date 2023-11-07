e2979e3d9940e9d9cdb5a96f322a5d7f7656e537
Jobs <yukeli@whale.im>
2023-11-07 20:13:44 +0800

    Updates

--- index.d.ts
+++
@@ -1,7 +0,0 @@
-import { createImageWm, DefaultCreateWmProps } from "./createWmSingle";
-interface CreateWmProps extends DefaultCreateWmProps {
-    repeat?: number;
-}
-declare const createWm: ({ container, width, height, textAlign, textBaseline, font, fillStyle, text, rotate, zIndex, repeat, }?: CreateWmProps) => () => void;
-declare const createWmSingle: ({ container, width, height, textAlign, textBaseline, font, fillStyle, text, rotate, zIndex, }?: DefaultCreateWmProps) => () => void;
-export { createWm, createWmSingle, createImageWm, CreateWmProps, DefaultCreateWmProps, };
